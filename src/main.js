eaaf15ae053622f5ccef634798e9e0cdfc13459c
Suizhazha <573505403@qq.com>
2020-08-14 21:00:51 +0800

    初步筛选男女全部

--- src/main.js
+++ src/main.js
@@ -1,8 +1,57 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue/dist/vue.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+
+let id = 0
+
+//封装！！！
+const createUser = (name,gender)=>{
+  id+=1
+  return {id,name,gender}
+}
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  data() {
+    return {
+      users: [
+        createUser('随心','男'),
+        createUser('王和军','女'),
+        createUser("聂文俊",'男'),
+        createUser('隋鑫','男')
+      ],
+      displayUsers: []
+    }
+  },
+  created() {
+    this.displayUsers = this.users
+  },
+methods:{
+    showMale(){
+    this.displayUsers = this.users.filter(u => u.gender==='男')
+    },
+  showFemale(){
+    this.displayUsers = this.users.filter(u => u.gender==='女')
+  },
+  showAll(){
+    this.displayUsers = this.users
+  }
+},
+
+  // 如何给三个按钮加事件处理函数
+  // 思路一：点击之后改 users
+  // 思路二：使用 computed
+
+  template: `
+    <div>
+      <div>
+        <button @click="showAll">全部</button>
+        <button @click="showMale">男</button>
+        <button @click="showFemale">女</button></div>
+      <ul>
+        <!--v-for接的属性：元素和索引    note:  :key!!!-->
+        <li v-for="(u,index) in users" :key="index">{{u.name}} - {{u.gender}}</li>
+      </ul>
+    </div>
+  `
+}).$mount("#app");
